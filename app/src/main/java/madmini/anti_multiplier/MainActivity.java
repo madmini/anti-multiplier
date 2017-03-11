b01ef2d5b435b77db7b922464670aaecc82eeebb
madmini <minigugler@gmail.com>
2017-03-12 00:15:08 +0100

    eyy first change

--- app/src/main/java/madmini/anti_multiplier/MainActivity.java
+++ app/src/main/java/madmini/anti_multiplier/MainActivity.java
@@ -1,13 +1,24 @@
 package madmini.anti_multiplier;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "madmini.anti-multiplier.DIVIDEND";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void divide(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String dividend = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, dividend);
+        startActivity(intent);
     }
 }
