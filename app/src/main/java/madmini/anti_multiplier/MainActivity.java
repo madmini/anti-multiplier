1a15e89d565a58f232753f32ce92328cb0698e21
madmini <minigugler@gmail.com>
2017-03-14 00:25:24 +0100

    works just fine

--- app/src/main/java/madmini/anti_multiplier/MainActivity.java
+++ app/src/main/java/madmini/anti_multiplier/MainActivity.java
@@ -1,24 +1,59 @@
 package madmini.anti_multiplier;
 
-import android.content.Intent;
+import android.icu.math.BigDecimal;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "madmini.anti-multiplier.DIVIDEND";
+public class MainActivity extends AppCompatActivity implements View.OnFocusChangeListener {
+
+    private TextView resultField;
+    private EditText numField;
+    private EditText denomField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // enable dynamic resizing if keyboard is active
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        // set result field
+        this.resultField = (TextView) findViewById(R.id.resultText);
+
+        // add event listeners
+        (this.numField = (EditText) findViewById(R.id.editNumerator)).setOnFocusChangeListener(this);
+        (this.denomField = (EditText) findViewById(R.id.editDenominator)).setOnFocusChangeListener(this);
+
+        // call method manually
+        this.onFocusChange(null,false);
     }
 
-    public void divide(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String dividend = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, dividend);
-        startActivity(intent);
+    private double divide (double num, double denom) {
+        return num/denom;
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus) {
+            double num = Double.parseDouble(numField.getText().toString());
+            double denom = Double.parseDouble(denomField.getText().toString());
+
+            String result = "";
+            if (denom == 0) {
+                result = "= NaN";
+            } else {
+                // using regex to remove trailing zeroes
+                result = "= " + Double.toString(divide(num, denom)).replaceAll("\\.0+$","");
+            }
+
+            this.resultField.setText(result);
+        }
     }
 }
